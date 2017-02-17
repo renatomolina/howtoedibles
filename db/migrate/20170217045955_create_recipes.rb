7609c0546ddd0d886d87f55e0b8596db0677be59
Renato Molina <renatomolinat@gmail.com>
2017-02-16 21:26:54 -0800

    Add recipe

---
+++ db/migrate/20170217045955_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :ingredients
+      t.text :instructions
+      t.float :suggested_weed
+      t.float :suggested_portion
+      t.string :video
+
+      t.timestamps
+    end
+  end
+end
