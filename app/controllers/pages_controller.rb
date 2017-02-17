0e6b91c028496c3bdbcfd06d82322fd32f797606
Renato Molina <renatomolinat@gmail.com>
2017-02-16 22:29:13 -0800

    Hot fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-
   def show
-    @recipe = Recipe.find_by(name: params[:recipe_name]&.capitalize || 'Butter')
+    @recipe = Recipe.find_by(name: 'Butter')
   end
 end
