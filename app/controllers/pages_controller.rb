28dc2d519dc97a7805a76ce5ae17f09be7fd1579
Renato Molina <renatomolinat@gmail.com>
2017-02-17 23:15:24 -0800

    Put specific order in menu by id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def show
     @recipe = Recipe.find_by(slug: params[:recipe_slug] || 'butter')
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(id: :asc)
   end
 end
