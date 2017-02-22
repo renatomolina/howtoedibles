5727e5478c21b114dc770556a31f027be513eb82
Renato Molina <renatomolinat@gmail.com>
2017-02-21 22:16:54 -0800

    Add lets encrypt

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root 'pages#show'
   get '/:recipe_slug', to: 'pages#show', as: 'recipe'
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
 end
