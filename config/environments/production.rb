5b90e3d15f2c24f7c1d77e578c782ba6f791f04e
Renato Molina <renatomolinat@gmail.com>
2017-02-26 01:40:50 -0800

    add env variable in i18n

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.i18n.default_locale = ENV["COUNTRY"]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
