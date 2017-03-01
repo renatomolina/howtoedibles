fe8b33e7a7982c99e0c66144e0d1682b17e0d448
Renato Molina <renatomolinat@gmail.com>
2017-02-28 22:17:25 -0800

    Fix tab highlighting

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
   def tab_highlight?(recipe_slug, recipe)
-    'active' if recipe_slug == recipe.slug || recipe_slug.nil? && recipe.slug == "butter"
+    'active' if recipe_slug == recipe.slug || recipe_slug.nil? && recipe.slug == t(:first_recipe)
   end
 end
