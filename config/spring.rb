ec1ff0919e862928322558fc6fd98a79c667de83
Renato Molina <renatomolinat@gmail.com>
2017-04-05 00:24:25 -0700

    trying to solve

--- config/spring.rb
+++ config/spring.rb
@@ -4,15 +4,3 @@
   tmp/restart.txt
   tmp/caching-dev.txt
 ).each { |path| Spring.watch(path) }
-
-Spring.after_fork do
-  if defined?(ActiveRecord::Base)
-    if Octopus.enabled?
-      Octopus.config[Rails.env.to_s]['master'] = ActiveRecord::Base.connection.config
-      ActiveRecord::Base.connection.initialize_shards(Octopus.config)
-    end
-
-    ActiveRecord::Base.establish_connection
-    QC.default_conn_adapter = QC::ConnAdapter.new(ActiveRecord::Base.connection.raw_connection)
-  end
-end
