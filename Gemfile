47627b9d2b2c249eb1cc543c94058828ee3e47c8
Renato Molina <renatomolinat@gmail.com>
2017-04-20 12:52:47 -0700

    fix

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'sitemap_generator'
 gem 'rails-i18n', '~> 5.0.0'
 gem 'ckeditor', github: 'galetahub/ckeditor'
 gem 'rollbar'
-gem 'rack-cors'
+gem 'rack-cors', :require => 'rack/cors'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,6 @@
-if defined? Rack::Cors
-  Rails.configuration.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins %w[
-         http://howtoedibles.com
-         http://www.howtoedibles.com
-         http://howtoedibles.herokuapp.com
-      ]
-      resource '/assets/*'
-    end
+config.middleware.insert_before 0, "Rack::Cors" do
+  allow do
+    origins '*'
+    resource '*', :headers => :any, :methods => [:get, :head, :options]
   end
 end
