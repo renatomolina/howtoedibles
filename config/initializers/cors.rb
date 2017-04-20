c64df28339015beafbdbb472b4c19b88c324e2b5
Renato Molina <renatomolinat@gmail.com>
2017-04-20 12:02:31 -0700

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,12 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-
-    resource '/assets/*',
-      headers: :any,
-      methods: [:get]
+if defined? Rack::Cors
+  Rails.configuration.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins %w[
+         http://howtoedibles.com
+         http://www.howtoedibles.com
+         http://howtoedibles.herokuapp.com
+      ]
+      resource '/assets/*'
+    end
   end
 end
