5a0f4907c284b383f6e725c3fc5c94eefc8c8c18
Renato Molina <renatomolinat@gmail.com>
2017-06-25 21:46:39 -0700

    adding s3 support

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
-Paperclip::Attachment.default_options[:url] = 'howtoedibles.s3-website-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:s3_host_name] = 'howtoedibles.s3-website-us-west-2.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
