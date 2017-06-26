b49abc456e1db495fe28c9ee32284b8fbe9f54e6
Renato Molina <renatomolinat@gmail.com>
2017-06-25 21:54:37 -0700

    fixing recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,8 @@ class Recipe < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
 
+  has_attached_file :photo
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
-  has_attached_file :photo, :default_url => "/images/missing.png"
 
   belongs_to :category
 
