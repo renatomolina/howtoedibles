ab000f2e7db939b47f479fc42003f12a9290d0a0
Renato Toth <renatomolinat@gmail.com>
2018-06-04 20:37:40 -0700

    adding https support to images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
 
-  has_attached_file :photo
+  has_attached_file :photo, s3_protocol: :https
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   belongs_to :category
