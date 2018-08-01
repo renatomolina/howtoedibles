9faa707f2568a77780958bfcf25c4d1cccc6a5de
Renato Toth <renatomolinat@gmail.com>
2018-07-31 21:34:19 -0700

    adding default procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,15 @@
-
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+preload_app!
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
-
-plugin :tmp_restart
-
-before_fork do
-  require 'puma_worker_killer'
-  PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
