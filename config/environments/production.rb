a0088a8f66efe8ad3aeadd9574a2915007574b6b
Renato Toth <renatomolinat@gmail.com>
2018-11-02 09:25:45 -0700

    update public_file_server headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Cache expiration - Leverage browser caching
-  config.public_file_server.headers = {
-    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
-    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
-  }
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=604800' }
 end
