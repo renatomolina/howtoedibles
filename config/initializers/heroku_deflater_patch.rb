f8db4bb019d1f78492bdefbd0e8a604bc57edef4
Renato Toth <renatomolinat@gmail.com>
2018-11-02 09:38:03 -0700

    adding cache configuration for heroku

---
+++ config/initializers/heroku_deflater_patch.rb
@@ -0,0 +1,11 @@
+module HerokuDeflater
+  class CacheControlManager
+    def cache_control_headers
+      if rails_version_5?
+        app.config.public_file_server.headers
+      else
+        app.config.static_cache_control
+      end
+    end
+  end
+end
