64621e28463c62e1164321d871ed18a1d651adaf
Renato Toth <renatomolinat@gmail.com>
2019-04-14 23:01:52 -0700

    remove unecessary code

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,11 +1,4 @@
 class PublicPagesController < ApplicationController
   def calculator
-    @recipes = Recipe.take(4)
-  end
-
-  private
-
-  def template_name
-    @template_name ||= params[:public_page]&.tr('-', '_')
   end
 end
