7f2a5f41272fc0c27c812e4712deaac623f60c5d
Renato Toth <renatomolinat@gmail.com>
2019-06-10 22:25:43 -0700

    testing one more robots txt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def robots
-    robots = File.read(Rails.root.join('public', "robots.#{ENV['APP_ENV']}.txt"))
+    robots = File.read(Rails.root.join('config', "robots.#{ENV['APP_ENV']}.txt"))
     render plain: robots
   end
 end
