664c64ca9e5ddb2ab5667b997a54f9322572cf99
Renato Toth <renatomolinat@gmail.com>
2019-06-20 21:23:32 -0700

    update titles and schema

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
     category = Category.friendly.find(params[:category_slug] || params[:category_id])
     @recipe = category.recipes.friendly.find(params[:recipe_slug] || params[:id])
     @suggested_portion = params[:portion] || @recipe.suggested_portion
-    @suggested_weed = params[:weed] || @recipe.suggested_weed
+    @suggested_weed = params[:weed] || @recipe.suggested_quantity
     @suggested_potency = params[:potency] || 14
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_184920) do
+ActiveRecord::Schema.define(version: 2019_06_14_041026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2018_12_19_184920) do
     t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
+  create_table "ebook_signups", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
@@ -53,7 +59,7 @@ ActiveRecord::Schema.define(version: 2018_12_19_184920) do
     t.string "name"
     t.text "ingredients"
     t.text "instructions"
-    t.float "suggested_weed"
+    t.float "suggested_quantity"
     t.float "suggested_portion"
     t.string "video"
     t.datetime "created_at", null: false
