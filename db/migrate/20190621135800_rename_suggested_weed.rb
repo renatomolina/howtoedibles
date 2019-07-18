7c74abb0786fcb77d6b89cc09cae0f4316c28db5
Renato Toth <renatomolinat@gmail.com>
2019-07-17 20:06:37 -0700

    fix the merge

--- db/migrate/20190621135800_rename_suggested_weed.rb
+++
@@ -1,5 +0,0 @@
-class RenameSuggestedWeed < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :recipes, :suggested_weed, :suggested_quantity
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_135800) do
+ActiveRecord::Schema.define(version: 2019_06_14_041026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_21_135800) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+  create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
