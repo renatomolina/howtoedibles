9960805348c9bf15d761a11897c8bd4a3d932ebf
Renato Toth <renatomolinat@gmail.com>
2019-11-21 10:01:03 -0800

    fixing rubocop issue

--- spec/models/ebook_signup_spec.rb
+++ spec/models/ebook_signup_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe EbookSignup, type: :model do
 
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
-    it { is_expected.to allow_value("email@addresse.foo").for(:email) }
-    it { is_expected.to_not allow_value("foo").for(:email) }
+    it { is_expected.to allow_value('email@addresse.foo').for(:email) }
+    it { is_expected.to_not allow_value('foo').for(:email) }
   end
 end
