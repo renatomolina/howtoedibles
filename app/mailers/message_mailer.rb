18abb226c7e950e763495fa19530e173658ba2a0
Rommel <rommel.osmiun@gmail.com>
2019-12-10 18:12:54 -0800

    Add contact page PR changes

---
+++ app/mailers/message_mailer.rb
@@ -0,0 +1,14 @@
+class MessageMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.message_mailer.contact.subject
+  #
+  def contact(message)
+    @greeting = 'Hi'
+    @body = "from: " + message.name + ", email: " + message.email + ", message: " + message.body
+
+    mail to: "howtoedibles@gmail.com", from: message.email
+  end
+end
