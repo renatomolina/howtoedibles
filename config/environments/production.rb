fd21ef7e746bc09a43f0f259ee5b76743986db09
Renato Toth <renatomolinat@gmail.com>
2020-04-04 13:56:01 -0700

    sending email with send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,16 +105,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'howtoedibles.com',
-    user_name:            ENV["GMAIL_EMAIL"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
-
-  ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => ENV['SENDGRID_DOMAIN'],
