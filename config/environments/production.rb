430193e1336826a88c56dac4fa937ee44977b23d
Renato Toth <renatomolinat@gmail.com>
2020-04-04 14:26:27 -0700

    changing port - integer to string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD'],
     domain: ENV['SENDGRID_DOMAIN'],
     address: 'smtp.sendgrid.net',
-    port: 465,
+    port: '465',
     authentication: :plain,
     enable_starttls_auto: true,
     tls: true 
