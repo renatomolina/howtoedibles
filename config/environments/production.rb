32b3d3f2f936d109a0a4b56c6a50ece0b2278033
Renato Toth <renatomolinat@gmail.com>
2020-04-04 13:43:45 -0700

    sending email with send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,14 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SENDGRID_DOMAIN'],
+    :address => 'smtp.sendgrid.net',
+    :port => 465,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
