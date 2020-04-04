823fd421c3ae00eedaec56719b2aecc6e87a1228
Renato Toth <renatomolinat@gmail.com>
2020-04-04 15:08:33 -0700

    changing port

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => ENV['SENDGRID_DOMAIN'],
   :address => 'smtp.sendgrid.net',
-  :port => 465,
+  :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
