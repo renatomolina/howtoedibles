cb5845e424b46d88b3ec4ab4d0adde3bf5a24086
Renato Toth <renatomolinat@gmail.com>
2020-12-15 22:02:06 -0800

    updating lazy load

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,4 +1,4 @@
 Lazyload::Rails.configure do |config|
   config.lazy_by_default = false
-  config.placeholder = "https://media.giphy.com/media/3oKIPEnIVoeC3iq1Y4/giphy.gif"
+  config.placeholder = "https://media.giphy.com/media/3o7bu3XilJ5BOiSGic/giphy.gif"
 end
