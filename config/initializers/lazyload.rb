343879f783d8bd29d171bd4b1a9a4b1b805f1110
Renato Toth <renatomolinat@gmail.com>
2020-12-15 21:59:37 -0800

    updating lazy load

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,4 +1,4 @@
 Lazyload::Rails.configure do |config|
   config.lazy_by_default = false
-  config.placeholder = "/assets/missing_recipe.jpg"
+  config.placeholder = "https://media.giphy.com/media/3oKIPEnIVoeC3iq1Y4/giphy.gif"
 end
