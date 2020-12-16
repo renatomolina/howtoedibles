bed17548b9650c3eb83960987e84b60825ad0c80
Renato Toth <renatomolinat@gmail.com>
2020-12-16 00:14:37 -0800

    change lazyload img

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,4 +1,4 @@
 Lazyload::Rails.configure do |config|
   config.lazy_by_default = false
-  config.placeholder = "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+  config.placeholder = "/missing-recipe.jpg"
 end
