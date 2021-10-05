a787ee99d990a01779476d68b5d083c56eeeb609
Renato Toth <renatomolinat@gmail.com>
2021-10-05 11:07:36 -0700

    adding cache config to aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,7 @@
 Aws.config.update({
 	  region: 'us-east-1',
 	  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+		cache_control: 'max-age=86400, private'
 })
 
 Aws::VERSION =  Gem.loaded_specs["aws-sdk"].version
