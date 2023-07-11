32a1a398e22abf7af79a89592036b1073d2968b0
Renato Molina Toth <renatomolinat@gmail.com>
2023-07-10 18:49:30 -0700

    remove invalid aws option

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,6 @@
 Aws.config.update({
 	  region: 'us-east-1',
-	  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-		cache_control: 'max-age=86400, private'
+	  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
 
 Aws::VERSION =  Gem.loaded_specs["aws-sdk"].version
