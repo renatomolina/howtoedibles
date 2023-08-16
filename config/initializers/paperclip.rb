e49acd752c38e3392a95aa359aedcf84ad3f3177
Renato Molina Toth <renatomolinat@gmail.com>
2023-08-15 21:04:37 -0700

    fixing paperclip config for heroku

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+unless Rails.env.development?
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
   Paperclip::Attachment.default_options[:default_url] = "missing_recipe.jpg"
